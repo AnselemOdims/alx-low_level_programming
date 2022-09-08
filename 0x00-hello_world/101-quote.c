8fbae017aa7ce4c54fb971fc8877cecb31c8db6a
Anselem <anselemodims@gmail.com>
2022-09-08 22:39:01 +0100

    Standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+char chr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(chr, 1, sizeof(chr), stderr);
+return (1);
+}
