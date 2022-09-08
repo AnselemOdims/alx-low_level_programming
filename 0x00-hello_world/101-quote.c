afc5df62131dadf30a34d26f0105594ca01b22d2
Anselem <anselemodims@gmail.com>
2022-09-08 22:51:39 +0100

    Standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,7 @@
 
 int main(void)
 {
-fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char chr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fprintf(stderr, "%s", chr);
 return (1);
 }
