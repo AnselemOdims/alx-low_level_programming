13898000a9efa3c77e90d49fdbabc0642678139a
Anselem <anselemodims@gmail.com>
2022-09-09 16:33:56 +0100

    Convert to lowercase except Q and E

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'program to print lowercase aphabets'
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+unsigned int i;
+char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+for (i = 0; i < strlen(alpha); ++i)
+{
+if (alpha[i] != 'Q' && alpha[i] != 'E')
+putchar(tolower((unsigned char) alpha[i]));
+}
+printf("\n");
+
+return (0);
+}
