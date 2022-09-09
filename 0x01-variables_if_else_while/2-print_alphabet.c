3ac0ca43145167547ec41e0f0708ec3975116f15
Anselem <anselemodims@gmail.com>
2022-09-09 15:29:20 +0100

    Convert to lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'program to print lowercase aphabets'
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+for (i = 0; i < strlen(alpha); ++i)
+{
+putchar(tolower((unsigned char) alpha[i]));
+}
+printf("\n");
+
+return (0);
+}
