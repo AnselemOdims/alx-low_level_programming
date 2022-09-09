cb47b361aac7644c69afd586f305eb3cf0fb3aa8
Anselem <anselemodims@gmail.com>
2022-09-09 16:47:06 +0100

    Print all single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'program decimal single digits'
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+int i = 0;
+
+while(i < 10)
+{
+putchar(i);
+}
+printf("\n");
+
+return (0);
+}
