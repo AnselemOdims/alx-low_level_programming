9bde8b55af3004b032453c255da622863e31b582
Anselem <anselemodims@gmail.com>
2022-09-09 17:44:59 +0100

    Print reverse lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'program to print lowercase aphabets in reverse'
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+char *str = "abcdefghijklmnopqrstuvwxyz";
+int i = strlen(str);
+
+for(;i > -1;i--)
+{
+putchar(str[i]);
+}
+putchar('\n');
+
+return (0);
+}
