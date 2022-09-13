e601d13b07d51d26957b5ee936cdfedcc06a9bb0
Anselem <anselemodims@gmail.com>
2022-09-13 15:04:54 +0100

    Add _islower function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-#include "_putchar.c"
+#include "main.h"
 
 /**
  * _islower - Lower?
@@ -12,23 +12,9 @@
 int _islower(int c)
 {
 int res;
+
 if (c >= 97 && c <= 122)
-res = 1;
+return (1);
 else
-res = 0;
-
-return (res);
-}
-
-/**
- * main - Entry point
- *
- * Description: 'program to call the function prototype'
- *
- * Return: 0
- */
-
-int main(void)
-{
-_islower('h');
+return (0);
 }
