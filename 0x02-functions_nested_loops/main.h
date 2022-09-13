399bc7710b7584d1f459783ed7e31890279479e1
Anselem <anselemodims@gmail.com>
2022-09-13 12:58:53 +0100

    Include header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,24 @@
-#ifndef "_putchar.c"
-#define "_putchar.c"
+#ifndef "putchar.c"
+#define "putchar.c"
+#endif
 #ifndef "0-putchar.c"
 #define "0-putchar.c"
+#endif
 #ifndef "1-alphabet.c"
 #define "1-alphabet.c"
+#endif
 #ifndef "2-print_alphabet_x10.c"
 #define "2-print_alphabet_x10.c"
+#endif
 #ifndef "3-islower.c"
 #define "3-islower.c"
+#endif
 #ifndef "4-isalpha.c"
 #define "4-isalpha.c"
+#endif
 #ifndef "5-sign.c"
 #define "5-sign.c"
+#endif
 #ifndef "6-abs.c"
 #define "6-abs.c"
 #endif
