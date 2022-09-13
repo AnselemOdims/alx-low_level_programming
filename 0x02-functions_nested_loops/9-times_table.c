eb58834982f73ecec3f2290b9602d07206a83366
Anselem <anselemodims@gmail.com>
2022-09-14 00:03:23 +0100

    Function to print times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * times_table - Times table
+ *
+ * Description: Print times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+int i = 0, count = 0, j;
+
+while (i < 10)
+{
+j = 0;
+
+while (j <= 9)
+{
+int res = j *count;
+
+if (res > 9)
+{
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+else
+{
+_putchar(res + '0');
+}
+
+if (j < 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+++j;
+}
+putchar('\n');
+
+++count;
+++i;
+}
+}
