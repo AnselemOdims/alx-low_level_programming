865596e9cfd1e32a7d6b2efaf5896fa88d91e263
Anselem <anselemodims@gmail.com>
2022-09-14 01:01:08 +0100

    Function to print times table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Add Function
+ *@x: integer param
+ *@y: integer param
+ *
+ * Description: Add teo integers
+ *
+ * Return: sum of the inputs
+ */
+
+int add(int x, int y)
+{
+return (x + y);
+}
