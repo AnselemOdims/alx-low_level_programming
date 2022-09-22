80b31f61c84722bd9d430ffd43472d43f58b7eef
Anselem <anselemodims@gmail.com>
2022-09-23 00:00:34 +0100

    Compare string function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *_strcmp - String compare function
+ *@s1: First string
+ *@s2: Second string
+ *
+ * Description: function to compare two strings
+ *
+ * Return: the difference of the first mistmatch
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int ct = 0, i, res;
+
+while (s1[ct])
+ct++;
+for (i = 0; i < ct && s1[i] != '\0'; i++)
+{
+if (s1[i] != s2[i])
+{
+res = s1[i] - s2[i];
+break;
+}
+}
+return (res);
+}
